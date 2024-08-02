36f68f39c1c50026e564498166a091770b5b1787
moebamba01 <djigal001@gmail.com>
2024-08-02 00:43:50 +0000

    starting

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React, { ReactNode } from 'react';
+
+const Rootlayout = ({children}: {children: ReactNode}) => {
+  return (
+    <main>
+      
+      {children}
+      
+    </main>
+  );
+}
+
+export default Rootlayout;
